c9ef2072e13f6dda9be5a738b2178026d5623285
iDuyz <119864780+iDuyz@users.noreply.github.com>
2023-12-18 22:08:45 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tugas UAS SPK
+Nama : Arya Yudi Prasetyo <br>
+NIM : 201011402133 <br>
+Kelas : 07-TPLP-012 <br>
+
+## Install requirements
+```pip install -r requirements.txt```
+
+## Run the app
+Untuk menjalankan:
+```python main.py```
---
+++ settings.py
@@ -0,0 +1,53 @@
+USER = 'postgres'
+PASSWORD = '123'
+HOST = 'localhost'
+PORT = '5432'
+DATABASE_NAME = 'UASiduy' 
+
+MEREK_SCALE = {
+  "POCO F5" : 8,
+  "POCO M5" : 7,
+  "POCO X5 Pro 5G" : 7,
+  "POCO X5 5G" : 6,
+  "POCO X3 Pro" : 8
+}
+
+DEV_SCALE_ram = {
+  "4 GB" : 6,
+  "6 GB" : 7,
+  "8 GB" : 8,
+  "16 GB" : 9
+}
+
+DEV_SCALE_prosesor = {
+  "Qualcomm Snapdragon 7+ Gen 2" : 7,
+  "MediaTek Helio G99" : 8,
+  "Qualcomm Snapdragon 778G" : 9,
+  "Qualcomm Snapdragon 695G" : 9,
+  "Qualcomm Snapdragon 860G" : 8
+}
+
+DEV_SCALE_storage = {
+  "64 GB" : 7,
+  "128 GB" : 8,
+  "256 GB" : 9
+}
+
+DEV_SCALE_baterai = {
+  "4.500 mAh" : 7,
+  "5.000 mAh" : 8,
+  "5.500 mAh" : 9,
+  "6.000 mAh" : 8
+}
+
+DEV_SCALE_harga = {
+  "Rp.1.299.000" : 6,
+  "Rp.2.999.000" : 7,
+  "Rp.3.999.000" : 8,
+  "Rp.4.999.000" : 9
+}
+
+DEV_SCALE_ukuran_layar = {
+  "6.5 inch" : 7,
+  "6.4 inch" : 8
+}
